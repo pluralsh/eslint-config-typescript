3e021345a7af39864ce362080f62ef55afd47fe2
Sebastian Florek <s.florek91@gmail.com>
2022-07-14 09:08:13 +0200

    Lint only ts/tsx files

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 module.exports = {
   root: true,
 
-  settings: {
-    // Allow importing more file types
-    'import/resolver': {
-      node: {
-        extensions: [
-          '.ts',
-          '.tsx',
-          '.json',
-        ],
+  overrides: [
+    {
+      files: ['**/*.{ts,tsx}'],
+      settings: {
+        // Allow importing more file types
+        'import/resolver': {
+          node: {
+            extensions: [
+              '.ts',
+              '.tsx',
+              '.json',
+            ],
+          },
+        },
+      },
+
+      extends: ['airbnb', 'airbnb-typescript', 'plugin:react/jsx-runtime'],
+      parser: '@typescript-eslint/parser',
+      env: {
+        browser: true,
+        node: true,
+        jest: true,
+      },
+
+      rules: {
+        '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+        'object-curly-newline': ['error', { multiline: true }],
+        'import/extensions': 'off',
       },
     },
-  },
-
-  extends: ['airbnb', 'airbnb-typescript', 'plugin:react/jsx-runtime'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { project: './tsconfig.json' },
-
-  env: {
-    browser: true,
-    node: true,
-    jest: true,
-  },
-
-  rules: {
-    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
-    'object-curly-newline': ['error', { multiline: true }],
-    'import/extensions': 'off',
-  },
+  ],
 };
